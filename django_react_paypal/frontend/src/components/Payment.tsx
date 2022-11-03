ac5aae6e3d1c087ecabae658a43e275c2e1bb96c
Benjamin Hadzic <benjaminhadzic@hotmail.com>
2022-11-03 20:19:35 +0100

    First commit

--- django_react_paypal/frontend/src/components/Payment.tsx
+++ django_react_paypal/frontend/src/components/Payment.tsx
@@ -6,11 +6,11 @@ export function Payment() {
 
     return (
         <div className="card">
-            <img src="https://images.unsplash.com/photo-1594498257673-9f36b767286c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80" alt="Airtable product" style={{ width: '100%' }} />
+            <img src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/158101-apps-news-what-is-klarna-and-how-does-it-let-you-buy-now-pay-later-with-no-interest-image1-fqkl3bal62.jpg" alt="iPhone 14" style={{ width: '100%' }} />
             <div className="card-details">
-                <h1>Airtable Product</h1>
-                <p className="price">$10.00</p>
-                <p>Some information about the product</p>
+                <h1>Mobile Phone</h1>
+                <p className="price">$100.00</p>
+                <p>Here is some information about the product that is being sold..</p>
                 <PayPalButtons
                     style={{ layout: "horizontal" }}
                     createOrder={(data, actions) => {
@@ -18,9 +18,9 @@ export function Payment() {
                             purchase_units: [
                                 {
                                     amount: {
-                                        value: "10.00"
+                                        value: "100.00"
                                     },
-                                    custom_id: "e-book-1234"  // the name or slug of the thing you're selling
+                                    custom_id: "Test Site Mobile Phone"  // the name or slug of the thing you're selling
                                 },
                             ],
                         });
